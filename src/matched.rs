1cfb42707d594a62c881320542cedc4f6fe223af
ms747 <mayur.shah.ha@gmail.com>
2020-06-24 20:48:17 +0530

    Add : Helper Methods

--- src/matched.rs
+++ src/matched.rs
@@ -1,4 +1,4 @@
-// TODO : Abtract matchedLines from main
+// TODO(#27) : Abtract matchedLines from main
 use std::collections::HashMap;
 
 type File = String;
@@ -16,11 +16,19 @@ impl MatchedLine {
             line: line.to_string(),
         }
     }
+
+    pub fn get_line(&self) -> &str {
+        self.line.as_str()
+    }
+
+    pub fn get_line_num(&self) -> usize {
+        self.line_num
+    }
 }
 
 impl std::fmt::Display for MatchedLine {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}:    {}", self.line_num, self.line)
+        write!(f, "{}:{}", self.line_num, self.line)
     }
 }
 
